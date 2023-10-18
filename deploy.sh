34242bb548355df7654f7a65195f0a701e8d00de
Ugochi Ukaegbu <110994907+Zenitugo@users.noreply.github.com>
2023-10-18 14:19:39 +0100

    Script to automate the deployment of Master and Slave VMs

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,65 @@
 #!/bin/bash
 
+#Initialise the process by running the command
+vagrant init ubuntu/focal64
+
+cat <<EOF  >  Vagrantfile
+Vagrant.configure("2") do |config|
+
+  config.vm.define "slave" do |subconfig|
+
+    subconfig.vm.hostname = "slave"
+    subconfig.vm.box = "ubuntu/focal64"
+    subconfig.vm.network "private_network", ip: "192.168.56.107"
+
+    subconfig.vm.provision "shell", inline: <<-SHELL
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt install sshpass -y
+    sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    sudo systemctl restart sshd
+    sudo apt-get install -y avahi-daemon libnss-mdns
+    SHELL
+  end
+
+  config.vm.define "master" do |subconfig|
+
+    subconfig.vm.hostname = "master"
+    subconfig.vm.box = "ubuntu/focal64"
+    subconfig.vm.network "private_network", ip: "192.168.56.106"
+
+    subconfig.vm.provision "shell", inline: <<-SHELL
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install -y avahi-daemon libnss-mdns
+    sudo apt install sshpass -y
+   # sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+   # sudo systemctl restart sshd
+    SHELL
+  end
+
+    config.vm.provider "virtualbox" do |vb|
+      vb.memory = "1024"
+      vb.cpus = "2"
+    end
+end
+EOF
+
+
+
+
 cluster='Vagrantfile'
-
-lamp='machines.sh'
-
-
 
 if [ -e $cluster ]
 then
 	echo "Starting 'Slave' VM..."
         vagrant up slave
-	bash $lamp
 
-
-         echo "Starting 'Master' VM..."
+        echo "Starting 'Master' VM..."
          vagrant up master
-	 bash $lamp
-	 vagrant ssh master
+	 vagrant ssh  master
 else
 	 echo "File not present"
 fi
 
 echo "Deployment complete!"
+
 
